9e604f4a06a04b0e397889f79894ffc0fdfcaa04
VastiDev <vastiane1@gmail.com>
2024-02-19 11:03:33 -0300

    finishing followed tutorial

--- src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.picpaysimplificado.picpaysimplificado.domain.user;
 
+import com.picpaysimplificado.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -27,4 +28,16 @@ public class User {
     @Enumerated(EnumType.STRING)
     private UserType userType;
 
+    public User(UserDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.document = data.document();
+        this.email = data.email();
+        this.password = data.password();
+        this.balance = data.balance();
+        this.userType = data.userType();
+
+
+
+    }
 }
--- src/main/java/com/picpaysimplificado/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/services/UserService.java
@@ -2,11 +2,14 @@ package com.picpaysimplificado.picpaysimplificado.services;
 
 import com.picpaysimplificado.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.picpaysimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -21,6 +24,22 @@ public class UserService {
         if(sender.getBalance().compareTo(amount) < 0){
             throw new Exception("Saldo negativo");
         }
+    }
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado!"));
+        }
 
+    public void saveUser(User user){
+    this.repository.save(user);
+    }
+
+    public User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.saveUser((newUser));
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.repository.findAll();
     }
 }
